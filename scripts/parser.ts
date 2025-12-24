fa6439b5b3d68d298aeafcaaa64b71659674c321
awesomearjun <iamawesomearjun@gmail.com>
2025-12-23 20:28:55 -0500

    Add json to store all sites

--- scripts/parser.ts
+++ scripts/parser.ts
@@ -6,15 +6,16 @@ import path from "path";
 
 const posts = fs.readdirSync(path.resolve(process.cwd(), "posts/markdown"));
 
-posts.forEach(post => {
+posts.forEach(async post => {
     const file = matter.read(path.join(path.resolve(process.cwd(), "posts/markdown"), post));
     const htmlFileName = post.replace(".md", ".html");
-    console.log(`HTML Filename: ${htmlFileName}`);
-    
+    let sites: { [key: string]: string } = {};
+    const htmlFilePath = path.resolve(process.cwd(), "posts/html", htmlFileName);
+
+    // Not a path to json, a path to the html file FOR the json entry
+    const jsonFilePath = `/${path.join(path.basename(process.cwd()), "posts/html", htmlFileName)}`;
 
     try {
-        const markdownString = file.content;
-
         const html = `
             <fieldset>
                 <legend>Metadata</legend>
@@ -25,8 +26,18 @@ posts.forEach(post => {
             ${marked.parse(file.content)}
             `;
 
-        writeFile(`${path.resolve(process.cwd(), "posts/html", htmlFileName)}`, html, { flag: "wx" });
+        await writeFile(htmlFilePath, html, { flag: "wx" });
+
+
+
+        sites[htmlFileName.replace(".html", "")] = jsonFilePath;
     } catch {
-        return;
+        sites[htmlFileName.replace(".html", "")] = jsonFilePath;
     }
+
+    fs.writeFile(`${path.resolve(process.cwd(), "public/sites.json")}`, JSON.stringify(sites, null, 2), err => {
+        if (err) {
+            console.error("Error writing to sites.json: ", err);
+        }
+    });
 });
