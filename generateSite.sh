49c07edca0df482dddbdeeecce826d334462aa85
awesomearjun <iamawesomearjun@gmail.com>
2025-12-23 20:28:28 -0500

    Add site boilerplate generator

---
+++ generateSite.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+# sorry windows and linux users, this is for mac only womp womp
+read "?Enter filename title: " title
+filename="$(date +%Y-%m-%d)-${title}.md"
+touch "$filename"
+echo "Created: $filename"
+echo "---
+title: ${title}
+date: $(date +%Y-%m-%d)
+description: <description>
+---
+# <site-title>" > "$filename"
+echo "Filled in template content."
+mv "$filename" "posts/markdown/"
+echo "Moved to posts/markdown directory."
+echo "Done! Access at posts/markdown/$filename"
